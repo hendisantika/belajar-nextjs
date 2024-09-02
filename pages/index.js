a0bb5b856f2408a6ea1912226df0fbc93dc563d8
hendisantika <hendisantika@yahoo.co.id>
2024-09-02 21:28:23 +0700

    feat: Day10 Assignment | Add Next Image🫸🌀✏️📗🐧🐳⬆

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,14 @@ export default function Home({children, metaTitle, metaDescription}) {
     return (
         <Layout metaTitle="Home" metaDescription="ini adalah halaman index">
             Index
+            <Image
+                src={profilePic}
+                alt="Picture of the author"
+                // width={500} otomatis di provide
+                // height={500} otomatis di provide
+                // blurDataURL="data:..." otomatis di provide
+                // placeholder="blur" // opsi blur-up ketika load image
+            />
         </Layout>
     )
 };
